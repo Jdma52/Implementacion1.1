c4b10cd1a9003200e13dd09edb43e3e54ea13924
paoaguilar1 <167396756+paoaguilar1@users.noreply.github.com>
2025-10-20 17:22:06 -0600

    Update Dashboard.js

--- petplaza/backend/models/Dashboard.js
+++ petplaza/backend/models/Dashboard.js
@@ -1,11 +1,1 @@
-const mongoose = require('mongoose');
 
-const DashboardSchema = new mongoose.Schema({
-  date: { type: Date, required: true, default: Date.now },
-  petsCount: { type: Number, default: 0 },
-  ownersCount: { type: Number, default: 0 },
-  appointmentsCount: { type: Number, default: 0 },
-  lowStockCount: { type: Number, default: 0 },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Dashboard', DashboardSchema);
