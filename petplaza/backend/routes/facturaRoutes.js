f1ffde43aab7ec53bede678dff9f4eef80245027
bely29 <anabelycoello13@gmail.com>
2025-10-29 01:48:42 -0600

    Update facturaRoutes.js

--- petplaza/backend/routes/facturaRoutes.js
+++ petplaza/backend/routes/facturaRoutes.js
@@ -1,15 +1,13 @@
-// facturaRoutes.js 
+// backend/routes/facturaRoutes.js
 const express = require("express");
 const router = express.Router();
 const facturaController = require("../controllers/facturaController");
 
-router.post("/", facturaController.createFactura);
-router.get("/", facturaController.getFacturas);
-router.get("/:id", facturaController.getFacturaById);
-router.delete("/:id", facturaController.deleteFactura);
-
-
-router.put("/:id/estado", facturaController.updateEstadoFactura);
+router.get("/", facturaController.obtenerFacturas);
+router.post("/", facturaController.crearFactura);
+router.put("/:id", facturaController.actualizarFactura);
+router.put("/:id/estado", facturaController.actualizarEstadoFactura);
+router.delete("/:id", facturaController.eliminarFactura);
+router.get("/loteActivo", facturaController.estadoLoteActivo);
 
 module.exports = router;
-
